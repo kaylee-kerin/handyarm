22b62e6e0476fbd6efe001dc23ea4084072e0355
Kaylee Kerin <kaylee@bootseg.com>
2025-02-12 00:45:01 -0500

    Add a package index, so imports work correctly... Sync the license

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MemoryBus:require('./src/bus.js').MemoryBus,
+  ROM:require('./src/rom.js').ROM,
+  RAM:require('./src/ram.js').RAM,
+  DMA:require('./src/dma.js').DMA,
+  IO:require('./src/io.js').IO,
+  Block_Input:require('./src/block_input.js').Block_Input,
+  Block_Output:require('./src/block_output.js').Block_Output
+}
